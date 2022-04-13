395dbfb6d70264a70a575bd2620fe7fb792d36e9
David Featherston <david@bjmdesign.com.au>
2022-04-13 12:00:39 +1000

    Add link to column on save

--- src/blocks/grid-column/save.js
+++ src/blocks/grid-column/save.js
@@ -1,8 +1,19 @@
-import { useBlockProps, useInnerBlocksProps } from "@wordpress/block-editor";
+import {useBlockProps, useInnerBlocksProps} from "@wordpress/block-editor";
+import {getGridColumnClasses} from "../../helpers";
+import {__} from '@wordpress/i18n'
 
-export default function Save() {
-    const blockProps = useBlockProps.save({ className: "bloc-grid-column" });
+export default function Save({attributes}) {
+    const {linkUrl, linkTarget} = attributes;
+    const blockProps = useBlockProps.save({className: getGridColumnClasses(attributes)});
     const innerBlocksProps = useInnerBlocksProps.save(blockProps);
 
-    return <div {...innerBlocksProps} />;
+    return <div {...blockProps}>
+        {linkUrl && <a
+            className="blokki-grid-column-link"
+            href={linkUrl}
+            target={linkTarget}
+            aria-label={__(`Go to ${linkUrl}`, 'blokki')}
+        ></a>}
+        <div {...innerBlocksProps} />
+    </div>
 }
